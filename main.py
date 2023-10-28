7386b42cd0206d9fe8220da908d6b64f35e45aa4
PlumLariat <65928319+PlumLariat@users.noreply.github.com>
2023-10-27 19:37:49 -0700

    added basic menubar

--- main.py
+++ main.py
@@ -6,5 +6,44 @@ root.title("Quest Tracker")
 root.geometry("640x512")
 
 
-root.mainloop()
+# quest menu functions
+def add_quest():
+    return
 
+
+def add_detailed_quest():
+    return
+
+
+def view_deleted_quests():
+    return
+
+
+# help menu segments
+def help_menu_help():
+    return
+
+
+def help_menu_about():
+    return
+
+
+# menubar segment
+menuBar = Menu(root)
+# quest menu segment
+questMenu = Menu(menuBar, tearoff=0)
+questMenu.add_command(label="Add Basic Quest", command=add_quest)
+questMenu.add_command(label="Add Detailed Quest", command=add_detailed_quest)
+questMenu.add_command(label="View Deleted Quests", command=view_deleted_quests)
+questMenu.add_separator()
+questMenu.add_command(label="Exit", command=root.destroy)
+menuBar.add_cascade(label="Quests", menu=questMenu)
+
+# help menu segment
+helpMenu = Menu(menuBar, tearoff=0)
+helpMenu.add_command(label="About", command=help_menu_about)
+helpMenu.add_command(label="Help", command=help_menu_help)
+menuBar.add_cascade(label="Help", menu=helpMenu)
+
+root.config(menu=menuBar)
+root.mainloop()
