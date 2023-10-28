f25b8951bd67385cf4c0e0320f3cdb4183927d41
PlumLariat <65928319+PlumLariat@users.noreply.github.com>
2023-10-28 15:05:23 -0700

    Created the add quest form, added app icon

--- main.py
+++ main.py
@@ -3,23 +3,57 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
+from tkinter.ttk import Combobox
 
 root = Tk()
 root.title("D&D Quest Tracker")
+icon = PhotoImage(file='./assets/dragon-head.png')
+root.iconphoto(True, icon)
 root.geometry("640x512")
 root.resizable(width=False, height=False)
 
 
 # quest menu functions
 def add_quest():
+
+    top = Toplevel()
+    top.resizable(width=False, height=False)
+    top.title('Add A Basic Quest')
+    top.iconphoto(True, icon)
+
+    add_quest_frame = Frame(top)
+    add_quest_frame.pack()
+
+    quest_title_frame = LabelFrame(add_quest_frame, text="Quest Title")
+    quest_title_frame.grid(row=0, column=0)
+    quest_title_label = Label(quest_title_frame, text="Title:")
+    quest_title_label.grid(row=0, column=0)
+    quest_title_input_field = Entry(quest_title_frame, width=60)
+    quest_title_input_field.grid(row=0, column=1)
+
+    quest_description_frame = LabelFrame(add_quest_frame, text="Description:")
+    quest_description_frame.grid(row=1, column=0)
+    quest_description_text = Text(quest_description_frame, width=60, height=10)
+    quest_description_text.grid(row=2, column=0)
+
+    quest_objectives = []
+    quest_objectives_frame = LabelFrame(add_quest_frame, text="Objectives:")
+    quest_objectives_frame.grid(row=2, column=0)
+    quest_objectives_combo = Combobox(quest_objectives_frame, values=quest_objectives, width=60)
+    quest_objectives_combo.grid(row=0, column=0)
+    quest_objectives_add_button = Button(quest_objectives_frame, text="Add",padx=10)
+    quest_objectives_add_button.grid(row=0, column=1)
+
     return
 
 
 def add_detailed_quest():
+    messagebox.showerror("Oops", "Get fucked I haven't made this part yet.")
     return
 
 
 def view_deleted_quests():
+    messagebox.showerror("Oops", "Double get fucked I haven't made this part yet either.")
     return
 
 
@@ -55,7 +89,7 @@ mf = Frame(root)
 mf.pack(fill=BOTH, expand=1)
 # Create a canvas
 canvas = Canvas(mf)
-canvas.pack(side=LEFT, fill=BOTH, expand=1)
+canvas.pack(side="top", fill=BOTH, expand=1)
 # Add a scrollbar to the canvas
 sb = ttk.Scrollbar(mf, orient=VERTICAL, command=canvas.yview)
 sb.pack(side=RIGHT, fill=Y)
@@ -68,7 +102,7 @@ second_frame = Frame(canvas)    # When adding an item to the scrollbar add it to
 canvas.create_window((0, 0), window=second_frame, anchor="nw")
 
 # This is the button that is always there on first load to guide quest creation
-Button(second_frame, text="Add A Basic Quest").pack()
+Button(second_frame, text="Add A Basic Quest", background='red', command=add_quest).pack()
 
 root.config(menu=menuBar)
 root.mainloop()
