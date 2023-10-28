d8ccb564dc21d28364e8e99790ee990c13d95e94
PlumLariat <65928319+PlumLariat@users.noreply.github.com>
2023-10-27 22:10:32 -0700

    updated scrollbar

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 
 from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk
 
 root = Tk()
 root.title("D&D Quest Tracker")
 root.geometry("640x512")
+root.resizable(width=False, height=False)
 
 
 # quest menu functions
@@ -47,10 +49,26 @@ helpMenu.add_command(label="About", command=help_menu_about)
 helpMenu.add_command(label="Help", command=help_menu_help)
 menuBar.add_cascade(label="Help", menu=helpMenu)
 
-# scrollbar def
-sb = Scrollbar(root)
+# Scrollbar Segment
+# Create a main frame
+mf = Frame(root)
+mf.pack(fill=BOTH, expand=1)
+# Create a canvas
+canvas = Canvas(mf)
+canvas.pack(side=LEFT, fill=BOTH, expand=1)
+# Add a scrollbar to the canvas
+sb = ttk.Scrollbar(mf, orient=VERTICAL, command=canvas.yview)
 sb.pack(side=RIGHT, fill=Y)
+# configure the canvas
+canvas.configure(yscrollcommand=sb.set)
+canvas.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
+# create another frame inside canvas
+second_frame = Frame(canvas)    # When adding an item to the scrollbar add it to the second frame
+# add new frame to a window in the canvas
+canvas.create_window((0, 0), window=second_frame, anchor="nw")
 
+# This is the button that is always there on first load to guide quest creation
+Button(second_frame, text="Add A Basic Quest").pack()
 
 root.config(menu=menuBar)
 root.mainloop()
